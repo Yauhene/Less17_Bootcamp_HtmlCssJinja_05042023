8d980a002cb420d3d74f63548e53e452ac8a3b85
Yauheni Aviltsau <XOPEK-007@yandex.ru>
2023-05-04 23:29:16 +0300

    перед наследованием :wq
    :wq
    
    перед наследованием
    :wq
    q

---
+++ flask_bootcamp/flask_bootcamp/main3.py
@@ -0,0 +1,19 @@
+﻿from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+@app.route('/index')
+def main():
+    with open('file.txt','r', encoding = 'utf-8') as file:
+        resultData = list()
+        for line in file.readlines():
+            resultData.append((tuple(line.split('\n')[0].split(';'))))
+            
+    return render_template('base3.html', data=resultData)
+
+
+if __name__ == '__main__':
+    app.run()
+
